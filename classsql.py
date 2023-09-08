1fe2478b028344cc4d10e6b2461987086032e51d
kumarjitdron <kumarjitdron69@gmail.com>
2023-09-08 21:33:25 +0530

    2nd commit

--- classsql.py
+++ classsql.py
@@ -2,7 +2,7 @@ import mysql.connector
 mydb=mysql.connector.connect(
 	host='localhost',
 	user='root',
-	password='9507910150'
+	password='Password'
 	)
 cur=mydb.cursor()
 s1="create database class"
